2ebaa15ae3be0b14bc2884e0e01f5bc61fa63484
Yakovka1 <yakovv59@gmail.com>
2023-03-26 14:12:46 +0700

    Lesson 3

---
+++ Lesson 3/Doctors.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3
+{
+    public abstract class Doctor
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Specialization { get; set; }
+        public string Experience { get; set; }
+
+        public virtual void Information()
+        {
+            Console.WriteLine($"\nИмя врача: {Name}\nВозраст: {Age}\nСпециализация: {Specialization}\nОпыт работы: {Experience}");
+        }
+
+        public virtual void Treatment(Patient patient)
+        {
+            if (new Random().Next(0, 11) > 2)
+            {
+                patient.Sickness = false;
+                Console.WriteLine($"\nВрач {Name} провел успешное лечение пациенту {patient.Name}\nПациент выздоровел");
+            }
+            else
+            {
+                patient.Sickness = true;
+                Console.WriteLine($"\nВрач {Name} не смог провести успешное лечение пациента {patient.Name}\nПациент не выздоровел");
+            }
+        }
+    }
+    public class Therapist : Doctor
+    {
+        public int DoctorsDistrict { get; set; }
+
+        public override void Information()
+        {
+            base.Information();
+            Console.WriteLine($"Рабочий участкок врача: {DoctorsDistrict}");
+        }
+    }
+
+    public class Ophthalmologist : Doctor
+    {
+        public override void Treatment(Patient patient)
+        {
+            if (new Random().Next(0, 11) > 3)
+            {
+                patient.Sickness = true;
+                Console.WriteLine($"\nВрач {Name} назначил успешное лечение пациенту {patient.Name}, скомпенсировавшее состояние пациента\nТем не менее этиологический фактор устранить не удалось");
+            }
+            else
+            {
+                patient.Sickness = true;
+                Console.WriteLine($"\nВрач {Name} не смог провести успешное лечение пациента {patient.Name}\nПациент направлен на оперативную корекцию глаза");
+            }
+        }
+    }
+
+    public class Neurolog : Doctor
+    {
+        public override void Treatment(Patient patient)
+        {
+            patient.Sickness = true;
+            Console.WriteLine($"\nНазначенное врачом {Name} лечение компенсирует состояние пациента {patient.Name}, однако перенесенное заболевание навсегда оставит свой след\nПациент остается больным");
+        }
+    }
+}
---
+++ Lesson 3/Program.cs
@@ -0,0 +1,77 @@
+﻿using Lesson_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var therapist1 = new Therapist
+        {
+            Name = "Александр Рева",
+            Age = 35,
+            Specialization = "Терапевт, пульманолог",
+            Experience = "2 года ординатуры и 9 лет работы в поликлинике",
+            DoctorsDistrict = 1
+        };
+
+        var ophthalmologist1 = new Ophthalmologist
+        {
+            Name = "Маргарита Гольдберг",
+            Age = 28,
+            Specialization = "Офтальмолог",
+            Experience = "2 года ординатуры и 2 года работы в поликлинике"
+        };
+
+        var neurolog1 = new Neurolog
+        {
+            Name = "Эльвира Хабибулина",
+            Age = 45,
+            Specialization = "Невролог",
+            Experience = "2 года ординатуры, 10 лет работы в поликлинике и 9 лет работы в ОКБ"
+        };
+
+        var patient1 = new TherapyPatient
+        {
+            Name = "Иван Иванов",
+            Age = 41,
+            Disease = "ОРЗ",
+            Insurance = "ОМС",
+            Sickness = true,
+            District = 1
+        };
+
+        var patient2 = new OphthalmicPatient
+        {
+            Name = "Дуся Ковальчук",
+            Age = 77,
+            Insurance = "ОМС",
+            Sickness = true,
+            Disease = "Миопия",
+            Vision = -1.5
+        };
+
+        var patient3 = new NeurologicalPatient
+        {
+            Name = "Владимир Дикий",
+            Age = 60,
+            Disease = "Алкогольная деменция",
+            Insurance = "ОМС",
+            Sickness = true,
+            MeningealSymptoms = false
+        };
+        patient3.Information();
+        patient3.DoctorsAppointment(neurolog1);
+        neurolog1.Information();
+        neurolog1.Treatment(patient3);
+
+        patient2.Information();
+        patient2.DoctorsAppointment(ophthalmologist1);
+        ophthalmologist1.Information();
+        ophthalmologist1.Treatment(patient2);
+
+        patient1.Information();
+        patient1.DoctorsAppointment(therapist1);
+        therapist1.Information();
+        therapist1.Treatment(patient1);
+    }
+}

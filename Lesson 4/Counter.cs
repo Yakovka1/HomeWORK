6173a602e919dcc0ec46bb1ff86e3b6c926fde1b
Yakovka1 <yakovv59@gmail.com>
2023-04-02 14:28:58 +0700

    Lesson 4

---
+++ Lesson 4/Counter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4
+{
+    internal class Counter
+    {
+        public delegate void Notifier();
+        public event Notifier Action;
+
+        public void Count()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i == 77)
+                {
+                    if (Action != null)
+                    {
+                        Action();
+                    }
+                }
+            }
+        }
+    }
+}
